8c39f9f7f0104dfccbaeef7557fb552e858d5847
kshitizvardhan <kshitizvardhan8@gmail.com>
2024-07-09 00:44:17 +0530

    Feat- render the searchBar component

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,43 @@
+import HeroCarousel from '@/Components/HeroCarousel'
+import SearchBar from '@/Components/SearchBar'
+import Image from 'next/image'
 import React from 'react'
 
 const Home = () => {
   return (
-    <div>Home</div>
+    <>
+      <section className='px-6 py-24 md:px-20'>
+        <div className='flex max-xl:flex-col gap-16'>
+          <div className='flex flex-col justify-center'>
+            <p className='small-text'>
+              Smart Shopping Starts Here:
+              <Image
+                src={"/assets/icons/arrow-right.svg"}
+                alt='arrow-right'
+                width={16}
+                height={16}
+              />
+            </p>
+            <h1 className='head-text'>
+            Transform Your Shopping Experience with
+              <span className='text-primary'> PriceWatch</span>
+            </h1>
+            <p className='mt-6'>
+            Unlock powerful, self-service product and growth analytics to boost conversions, engage effectively, and retain effortlessly.
+            </p>
+            <SearchBar/>
+          </div>
+          <HeroCarousel/>
+        </div>
+      </section>
+
+      <section className='trending-section'>
+        <h2 className='section-text'>Trending</h2>
+        <div className='flex flex-wrap gap-x-8 gap-y-16'>
+
+        </div>
+      </section>
+    </>
   )
 }
 
